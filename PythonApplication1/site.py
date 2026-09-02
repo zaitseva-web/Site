42b107d6c912c434c6d8a0ef954cea3214515507
zaitseva-web <sonazajceva56@gmail.com>
2026-09-02 14:47:23 +0300

    Add project files.

---
+++ PythonApplication1/site.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, session, url_for
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    name = " "
+
+    if request.method == 'POST':
+        session['name'] = request.form.get('user_input')
+    name = session.get('name', 'Guest')
+    return render_template('index.html', name=name)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+if __name__ == '__main__':
+    app.run(debug=True)
